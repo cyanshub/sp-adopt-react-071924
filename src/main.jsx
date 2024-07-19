663ff7128bbae1849830bb1a62a78c157d476ab6
travel15740 <travel15740@gmail.com>
2024-07-19 23:45:40 +0800

    準備 bootstrap 樣式與 style-components 可建立組件樣式的工具

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css' // 載入 bootstrap 框架樣式
+import { ThemeProvider } from 'styled-components' // 可以傳入 globals 參數的工具
+
+// 宣告 theme 變數
+const theme = {
+  colors: {
+    primary_300: '#ff7777',
+    primary_400: '#e33e3e',
+    primary_500: '#af0505',
+  }
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* // 包住 App，並自訂 theme 屬性, 並傳入 global 痊域參數 */}
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
