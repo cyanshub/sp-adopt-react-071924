98737dc4b2a9874aadede8d4ce1b3c44949f5d66
travel15740 <travel15740@gmail.com>
2024-08-25 09:52:53 +0800

    準備 bootstrap 樣式與 style-components 及 themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AdoptPage from './pages/AdoptPage'
 
+// 引入bootstrap框架樣式  
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 // 定義專案基礎路徑
 const basename = import.meta.env.VITE_PUBLIC_URL
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from 'styled-components' // 可以傳入 globals 參數的工具
+
+// 宣告 theme 變數
+const theme = {
+  colors: {
+    primary_300: '#ff7777',
+    primary_400: '#e33e3e',
+    primary_500: '#af0505'
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* // 包住 App，並自訂 theme 屬性, 並傳入 global 痊域參數 */}
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
